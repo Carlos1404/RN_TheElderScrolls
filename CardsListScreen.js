df0628506181605245e1d2d4072dea514f64137c
zigoval <rouby.v@hotmail.com>
2019-03-22 12:40:16 +0100

    init list page with navigation to details

--- CardsListScreen.js
+++ CardsListScreen.js
@@ -1,29 +1,53 @@
-import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, FlatList, Image, TouchableOpacity } from "react-native";
 
 class CardsListScreen extends React.Component {
   static navigationOptions = {
-    title: 'CardsList'
+    title: "CardsList"
   };
-    render() {
-      const {navigate} = this.props.navigation;
-      return (
-        <View style={styles.container}>
-          <Button
-            title="Go to Card Details"
-            onPress={() => this.props.navigation.navigate("CardDetails")}
-          />
-        </View>
-      );
-    }
+  state = {
+    cards: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.elderscrollslegends.io/v1/cards?pageSize=20")
+      .then(response => response.json())
+      .then(response => this.setState({ cards: response.cards }))
+      .catch(err => console.warn(err));
   }
 
-  export default CardsListScreen
+  renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("CardDetails", { card: item })}
+      >
+        <Text>{item.name}</Text>
+        <Image style={{ width: 100, height: 150 }} source={{ uri: item.imageUrl }} />
+      </TouchableOpacity>
+    );
+  };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.cards}
+          renderItem={this.renderItem}
+          numColumns={2}
+          style={styles.list}
+        />
+      </View>
+    );
+  }
+}
+
+export default CardsListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  list: {}
+});
